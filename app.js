a114eab1a58e26fa060eef56078a00d01fe0f0fa
eddy lee <192184280+Yongji-Li@users.noreply.github.com>
2025-03-10 17:09:48 +0900

    all

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello CTI!onepod-0310');
+  res.end('Hello! CTI!!');
 });
 
 server.listen(8081, () => {
