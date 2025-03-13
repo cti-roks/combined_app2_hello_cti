70a827a8f061a75c69630a22ec76761d4b024828
Yongji-Li <192184280+Yongji-Li@users.noreply.github.com>
2025-03-13 12:18:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! CTI!!');
+  res.end('Hello! CTI!!from log app');
 });
 
 server.listen(8081, () => {
